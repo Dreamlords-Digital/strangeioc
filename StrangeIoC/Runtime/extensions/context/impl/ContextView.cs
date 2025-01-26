ebdc98e0ebe4f851f7e9d10dbdcda0ea72cd7466
Ferdinand Joseph Fernandez <ferdinandjosephfernandez@gmail.com>
2025-01-26 22:58:47 +0800

    Band-aid fix for the "lest all hell break loose" exception

--- StrangeIoC/Runtime/extensions/context/impl/ContextView.cs
+++ StrangeIoC/Runtime/extensions/context/impl/ContextView.cs
@@ -16,13 +16,13 @@
 
 /**
  * @class strange.extensions.context.impl.ContextView
- * 
+ *
  * The Root View of a Context hierarchy.
- * 
+ *
  * Extend this class to create your AppRoot, then attach
  * that MonoBehaviour to the GameObject at the very top of
  * your display hierarchy.
- * 
+ *
  * The startup sequence looks like this:
 
 		void Awake()
@@ -39,10 +39,11 @@ using strange.extensions.context.api;
 
 namespace strange.extensions.context.impl
 {
+	[DefaultExecutionOrder(-2000)]
 	public class ContextView : MonoBehaviour, IContextView
 	{
 		public IContext context{get;set;}
-		
+
 		public ContextView ()
 		{
 		}
